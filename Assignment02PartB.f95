ac909ca7b7e8a6e2521ee49b595ad0a40fb52846
Nelius1k <corneillebobda@gmail.com>
2023-12-03 19:01:31 -0500

    ass2 completed

--- Assignment02PartB.f95
+++ Assignment02PartB.f95
@@ -10,13 +10,27 @@ program VehicleEntrySearch
         read(10,'(a)') line
     end do
 
+    open(11, file='output_suspects.txt', action='write')
     do 
-        read(10,100) make, color, plate, year, month, day, hr, min, sec
+        read(10,100, iostat=iost) make, color, plate, year, month, day, hr, min, sec
         if (iost /= 0) then
-            print*, "Error reading line:", iost
+            if (iost == -1) then
+                print*, "End of file reached."
+            else
+                print*, "Error reading line:", iost
+            end if
             exit
         end if
-        print*, make, color, plate
+
+        !write(*,100) make
+        
+        if ((make == 'Nissan') .and. (color == 'White') .and. (hr == 15)) then
+            !write(*,100) year, month, day, hr, min, sec
+            write(11,100) make, color, plate, year, month, day, hr, min, sec
+        end if
     end do
-    100 format(a8,1x,a7,3x,a9,1x,i5,1x,i3,1x,i2,1x,i3,1x,i3,1x,i3)
+    100 format(a8,1x,a7,3x,a9,1x,i4,2x,i1,1x,i2,1x,i2,1x,i2,1x,i2)
+
+    close(10)
+    close(11)
 end program VehicleEntrySearch
